f47c9e01bf728662d69ad67136bb46c638070afa
DARK DEVIL <108585506+Dark756@users.noreply.github.com>
2022-12-28 20:25:41 +0600

    add richpresence

---
+++ config.lua
@@ -0,0 +1,16 @@
+print "qb-richpresence made by Pengu Script"
+print "version 1.0"
+Config = {}
+
+Config.DiscordLink = "https://discord.gg/BpxwSQEQgP" -- Discord Server Join Link
+Config.FiveMConnectLink = "fivem://connect/localhost:30120" -- FiveM Server Connect Link
+Config.DiscordAppId = '1035842110429089822' -- Application Id Goes Here
+
+Config.LargeAssetName = 'large' -- Large/Main Asset Name
+Config.LargeAssetText = 'Join Today!' -- When You Hover Over The Icon, This Is What It Will Say
+
+Config.ServerName = 'DARK DEVIL' -- Server Name Here
+
+Config.SmallAssetEnabled = false -- Toggles Small Asset. (true/false)
+Config.SmallAsset = 'small' -- Small Asset Name
+Config.SmallAssetText = 'Server is Online' -- When You Hover Over The Icon, This Is What It Will Say
